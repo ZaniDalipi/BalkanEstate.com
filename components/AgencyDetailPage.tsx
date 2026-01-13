587bf2f3686395ee947f7bb7400868e6e75c7c74
ZaniDalipi <zanoin@gmail.com>
2026-01-14 00:58:18 +0100

    fixed a small issue with log removal

--- components/AgencyDetailPage.tsx
+++ components/AgencyDetailPage.tsx
@@ -1448,7 +1448,7 @@ const AgencyDetailPage: React.FC<AgencyDetailPageProps> = ({ agency }) => {
                       </div>
                     </div>
                   </div>
-                </div>
+                
               );
               })}
             </div>
