83e61f6746cf98dd192c104539eb394324a66539
ZaniDalipi <zanoin@gmail.com>
2026-01-17 17:48:36 +0100

    added icon to the constants file

--- constants.ts
+++ constants.ts
@@ -888,3 +888,18 @@ export const FunnelIcon: React.FC<{ className?: string }> = ({ className }) => (
         React.createElement('path', { strokeLinecap: "round", strokeLinejoin: "round", d: "M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" })
     )
 );
+
+export const PlayCircleIcon: React.FC<{ className?: string }> = ({ className }) => (
+    React.createElement(Icon, { className },
+        React.createElement('path', {
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            d: "M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.26a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+        }),
+        React.createElement('path', {
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            d: "M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        })
+    )
+);
