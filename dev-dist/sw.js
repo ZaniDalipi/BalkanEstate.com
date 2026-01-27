b4c7c064ee8c4dd9d581f33bf0def37a73063aad
ZaniDalipi <zanoin@gmail.com>
2026-01-27 19:15:33 +0100

    commiting some files that are not in the new branch sw.js update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.77id7cci6f8"
+    "revision": "0.bgu1gd933sc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
