1d7f6326b9a785f1f31c3deea1a3c0f2fe95e2e1
ZaniDalipi <zanoin@gmail.com>
2026-02-22 09:24:21 +0100

    deleted uneccesary files and updated service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,12 +82,12 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6akq5lligig"
+    "revision": "0.3a5glo7mmes"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
     allowlist: [/^\/$/],
-    denylist: [/^\/api\//]
+    denylist: [/^\/api\//, /^\/auth\/callback/]
   }));
   workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.CacheFirst({
     "cacheName": "google-fonts-cache",
